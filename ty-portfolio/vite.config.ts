96641fad632c226c574020ec1b797e9bb8ae7054
Taeyoon <taeyoonkim1995@gmail.com>
2025-02-12 18:14:40 -0800

    deploy fixes

--- ty-portfolio/vite.config.ts
+++ ty-portfolio/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/TYWebv2",
+	base: "/TYWebv2/",
 	plugins: [react()],
 	css: {
 		postcss: "./postcss.config.js",  // PostCSS 설정 경로 추가
