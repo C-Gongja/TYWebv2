8eec2526258608151fbec923636ee19433c69c6f
Taeyoon <taeyoonkim1995@gmail.com>
2025-02-12 18:53:45 -0800

    s

--- ty-portfolio/src/App.tsx
+++ ty-portfolio/src/App.tsx
@@ -5,23 +5,28 @@ import Layout from './components/layout';
 import MainPage from './routes/main-page';
 import Resume from './routes/resume';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			// element: <ProtectedRoute><Layout /></ProtectedRoute>,
+			element: <Layout />,
+			children: [
+				{
+					path: "",
+					element: <MainPage />,
+				},
+				{
+					path: "resume",
+					element: <Resume />,
+				},
+			]
+		},
+	],
 	{
-		path: "/",
-		// element: <ProtectedRoute><Layout /></ProtectedRoute>,
-		element: <Layout />,
-		children: [
-			{
-				path: "",
-				element: <MainPage />,
-			},
-			{
-				path: "/resume",
-				element: <Resume />,
-			},
-		]
-	},
-]);
+		basename: '/TYWebv2',
+	}
+);
 
 function App() {
 	const [isLoading, setIsLoading] = useState(true);
