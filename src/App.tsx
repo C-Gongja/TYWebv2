a1b5c44a19cc9b1ed5c6aab2f62131a58420d7f3
Taeyoon <taeyoonkim1995@gmail.com>
2025-02-12 23:05:58 -0800

    /

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,12 @@ import Layout from './components/layout';
 import MainPage from './routes/main-page';
 import Resume from './routes/resume';
 
-// const router = createBrowserRouter([
-// 	{
-// 		path: "/",
-// 		element: <Layout />,
-// 		children: [
-// 			{
-// 				path: "",
-// 				element: <MainPage />,
-// 			},
-// 			{
-// 				path: "resume",
-// 				element: <Resume />,
-// 			},
-// 		],
-// 		basename: process.env.NODE_ENV === 'production' ? '/TYWebv2' : '/',
-// 	},
-// ]);
-
 function App() {
 	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		console.log(process.env.NODE_ENV);
+	});
+
 	useEffect(() => {
 		// init();
 		setIsLoading(false);
