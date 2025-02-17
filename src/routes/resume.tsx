a9f7e171cb5c05172bfdcb72d140ccfb5f418c24
Taeyoon <taeyoonkim1995@gmail.com>
2025-02-16 17:32:00 -0800

    Update resume.tsx

--- src/routes/resume.tsx
+++ src/routes/resume.tsx
@@ -7,10 +7,10 @@ export default function Resume() {
 	}, []);
 	return (
 		<div id="resume" className="pt-[120px] pb-[0px] px-[5%] md:px-[10%] xl:px-[20%] bg-transparent">
-			<h1 className="flex justify-center items-center text-6xl text-white font-bold m-[50px]">Resume</h1>
+			<h1 className="flex justify-center items-center text-6xl text-gray-200 font-bold m-[50px]">Resume</h1>
 			<div className="flex justify-center items-center">
 				<iframe
-					src="./assest/files/resume.pdf"
+					src="./assest/files/Resume.pdf"
 					width="100%"
 					height="800px"
 					className="border-4 border-gray-300"
